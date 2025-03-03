63a3219728598409dd9353a28341a94ac16ad3cf
nknandakuamr <tec.thunder2022nk@gmail.com>
2025-03-03 22:24:33 +0530

    Complited Bookmark and Notes Routes Configuration

--- backend/controllers/BookMarkController.js
+++ backend/controllers/BookMarkController.js
@@ -1,6 +1,38 @@
 import db from "../config/db.js";
 db.connect();
-export const getBookMarks = async (req, res) => {};
-export const createBookMark = async (req, res) => {};
-export const updateBookMark = async (req, res) => {};
-export const deleteBookMark = async (req, res) => {};
+export const getBookMarks = async (req, res) => {
+    try {
+        const response = await db.query("SELECT * FROM bookmark ORDER BY created_date DESC");
+        res.status(200).json(response.rows);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const createBookMark = async (req, res) => {
+    const {title, url, category} = req.body;
+    try {
+        const response = await db.query("INSERT INTO bookmark (title, url, category) VALUES ($1, $2, $3) RETURNING *", [title, url, category]);
+        res.status(201).json(response.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const updateBookMark = async (req, res) => {
+    const id = req.params.id;
+    const {title, url, category} = req.body;
+    try {
+        const response = await db.query("UPDATE bookmark SET title = $1, url = $2, category = $3 WHERE id = $4 RETURNING *", [title, url, category, id]);
+        res.status(200).json(response.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const deleteBookMark = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await db.query("DELETE FROM bookmark WHERE id = $1", [id]);
+        res.status(200).json({ message: "Bookmark was deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
--- backend/controllers/NoteController.js
+++ backend/controllers/NoteController.js
@@ -1,6 +1,38 @@
 import db from "../config/db.js";
 db.connect();
-export const getNotes = async (req, res) => {};
-export const createNote = async (req, res) => {};
-export const updateNote = async (req, res) => {};
-export const deleteNote = async (req, res) => {};
+export const getNotes = async (req, res) => {
+    try {
+        const response = await db.query("SELECT * FROM note ORDER BY created_date DESC");
+        res.status(200).json(response.rows);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const createNote = async (req, res) => {
+    const {title, content,category} = req.body;
+    try {
+        const response = await db.query("INSERT INTO note (title, content, category) VALUES ($1, $2, $3) RETURNING *", [title, content, category]);
+        res.status(201).json(response.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const updateNote = async (req, res) => {
+    const id = req.params.id;
+    const {title, content, category} = req.body;
+    try {
+        const response = await db.query("UPDATE note SET title = $1, content = $2, category = $3 WHERE id = $4 RETURNING *", [title, content, category, id]);
+        res.status(200).json(response.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
+export const deleteNote = async (req, res) => {
+    const id = req.params.id;
+    try {
+        await db.query("DELETE FROM note WHERE id = $1", [id]);
+        res.status(200).json({ message: "Note was deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Something went wrong. Please try again" });
+    }
+};
